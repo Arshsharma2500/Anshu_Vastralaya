1b414b009c022d142afd79ec36e5d05fcd9b2dab
Arsh Kumar <saschoogle@gmail.com>
2024-07-06 22:20:15 +0530

    modification in utilities

--- server/utilities.js
+++ server/utilities.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-
+// jwt token
 function authenticateToken(req,res,next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
