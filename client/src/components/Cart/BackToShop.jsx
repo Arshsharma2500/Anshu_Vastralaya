3bd96a9a9f12d9feac6db6b95c657c1be10d8a66
Arsh Kumar <saschoogle@gmail.com>
2024-07-12 22:06:07 +0530

    improve UI

--- client/src/components/Cart/BackToShop.jsx
+++ client/src/components/Cart/BackToShop.jsx
@@ -11,7 +11,7 @@ const MoveArrowButton = () => {
     return (
         <button 
             className='flex flex-wrap gap-2 items-center p-2'
-            onClick={handleClick}
+            onClick={handleClick} // here function is for handleclick
         >
             <GoArrowLeft className={`bg-gray-200 text-orange-700 transform scale-100 transition-transform duration-300 ${arrowPosition ? '-translate-x-4' : 'translate-x-0'}`} />
             <p className='text-orange-700 font-serif font-lg'>Back To Shop</p>
