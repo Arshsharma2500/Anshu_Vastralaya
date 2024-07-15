d7e964c6eed648e7c1577d4912acae34d32bb466
Arsh Kumar <saschoogle@gmail.com>
2024-07-15 23:37:34 +0530

    updated

--- client/src/components/payment/Payement.jsx
+++ client/src/components/payment/Payement.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Payement() {
   return (
     <div className='flex flex-wrap justify-center text-3xl py-28 font-serif'>It's take some time to develop.. so Please make payement on phone number e.t.c 
-      ThankYou!
+      ThankYou!....
     </div>
   )
 }
